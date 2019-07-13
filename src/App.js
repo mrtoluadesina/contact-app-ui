ad12096f8c5a72f60726c3daaa85a9c8cc27a7cd
Tolu Adesina <tadesina90@gmail.com>
2019-07-13 15:27:02 +0100

    App.js updated with initial code for the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from '@reach/router';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { RestLink } from 'apollo-link-rest';
+
+import Header from './components/Header';
+import Contacts from './components/Contacts';
+import Blocked from './components/Blocked';
+import CreateContact from './components/CreateContact';
+
 import './App.css';
+
+const restLink = new RestLink({ uri: "https://swapi.co/api/" });
+
+// setup your client
+const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Header />
+        <Router>
+          <Contacts path="/" />
+          <Blocked path="/blocked" />
+          <CreateContact path="/create-contact" />
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
